8f3446ea38d6bcb8bd3d1832395c4655d0cbf6f3
jman420 <jgiannone@mdsol.com>
2023-10-21 22:22:22 -0400

    Root All Hosts   - Roots all available rootable hosts on the wide network

---
+++ src/scripts/root-all-hosts.ts
@@ -0,0 +1,32 @@
+import {NS} from '@ns';
+
+import {LoggerMode, getLogger} from '/scripts/logging/loggerManager';
+import {
+  SECTION_DIVIDER,
+} from '/scripts/logging/logOutput';
+import { scanWideNetwork } from '/scripts/workflows/recon';
+import { getRootTools, obtainRoot } from '/scripts/workflows/escalation';
+
+/** @param {NS} netscript */
+export async function main(netscript: NS) {
+  const logWriter = getLogger(
+    netscript,
+    'root-all-hosts',
+    LoggerMode.TERMINAL
+  );
+  logWriter.writeLine('Root All Available Hosts');
+  logWriter.writeLine(SECTION_DIVIDER);
+
+  logWriter.writeLine('Getting all rootable hosts...');
+  const availableHosts = scanWideNetwork(netscript, false, false);
+  const rootTools = getRootTools(netscript);
+  const rootableHosts = availableHosts.filter(host => !netscript.hasRootAccess(host) && netscript.getServerNumPortsRequired(host) <= rootTools.length);
+  logWriter.writeLine(`Found ${rootableHosts.length} rootable hosts...`);
+  logWriter.writeLine(SECTION_DIVIDER);
+
+  for (const hostname of rootableHosts) {
+    logWriter.writeLine(`Obtaining root on ${hostname}`);
+    obtainRoot(netscript, hostname);
+  }
+  logWriter.writeLine('Successfully obtained root on all rootable hosts.');
+}
